e5624cdbffc88731a3de3e9b966d8f9dd95dd3e6
vincentpier <gyurgyikv@gmail.com>
2023-11-16 13:28:39 -0500

    add a check for image in index, add a placeholder image if no image, recent prompts

--- script-index.js
+++ script-index.js
@@ -11,8 +11,21 @@ const displayDailyImage = () => {
     const imageUrl = `images/${currentDate}.png`;
     const imageElement = document.getElementById('daily-image');
     const keywordsElement = document.getElementById('keywords');
-    imageElement.src = imageUrl;
-    keywordsElement.innerText = `Prompt: ${keywords} Date: ${currentDate}`;
+
+    // Check if image exists (if site is loaded before script runs for day there will be no image)
+    fetch(imageUrl)
+        .then(response => {
+            if (response.ok) {
+                imageElement.src = imageUrl;
+                keywordsElement.innerText = `Prompt: ${keywords} Date: ${currentDate}`;
+            } else {
+                imageElement.src = 'images/noImage.png'
+                keywordsElement.innerText = `No image available for ${currentDate}, come back later!`
+
+                // Add 'no-image' class so we can adjust size
+                imageElement.classList.add('no-image');
+            }
+        })
 };
 if (document.getElementById('image-container')) {
     // This is index.html
